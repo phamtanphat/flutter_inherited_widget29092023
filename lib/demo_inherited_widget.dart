dd1f321f058f9dc1c4a17284c36c38dca0c1c8ec
phamtanphat <phatdroid94@gmail.com>
2023-11-03 20:14:52 +0700

    setup ui

--- lib/demo_inherited_widget.dart
+++ lib/demo_inherited_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DemoInheritedWidget extends StatefulWidget {
@@ -14,7 +16,83 @@ class _DemoInheritedWidgetState extends State<DemoInheritedWidget> {
       appBar: AppBar(
         title: Text("Demo inherited widget"),
       ),
-      body: Container(),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Ongba(
+          child: Chame(
+            child: Concai(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Ongba extends StatefulWidget {
+  Widget child;
+  Ongba({required this.child});
+
+  @override
+  State<Ongba> createState() => _OngbaState();
+}
+
+class _OngbaState extends State<Ongba> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("Ong ba widget"),
+        Text("Number random = $number"),
+        ElevatedButton(onPressed: () {
+           setState(() {
+             number = Random().nextInt(1000);
+           });
+        }, child: Text("Random number")),
+        widget.child
+      ],
+    );
+  }
+}
+
+
+class Chame extends StatefulWidget {
+  Widget child;
+  Chame({required this.child});
+
+  @override
+  State<Chame> createState() => _ChameState();
+}
+
+class _ChameState extends State<Chame> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("Cha me widget"),
+        widget.child
+      ],
+    );
+  }
+}
+
+class Concai extends StatefulWidget {
+  const Concai({super.key});
+
+  @override
+  State<Concai> createState() => _ConcaiState();
+}
+
+class _ConcaiState extends State<Concai> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Con cai widget"),
     );
   }
 }
