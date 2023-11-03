29a30588435233790da6ad907c6626134c1b2d30
phamtanphat <phatdroid94@gmail.com>
2023-11-03 21:24:16 +0700

    future

--- lib/demo_asynchronous.dart
+++ lib/demo_asynchronous.dart
@@ -1,9 +1,24 @@
+import 'dart:math';
+
 void main() {
+  // a = 5
+  // b = 10
+  // result = a + b - 3 + b - 1
+  var a = 5;
+  var b = 10;
 
-  var future = Future.delayed(Duration(seconds: 2), () {
-    throw Exception("Loi");
-  });
+  plus(a, b)
+      .then((total1) => minus(total1, 3))
+      .then((total2) => Future.error(Exception("Loi total 2")))
+      .then((total3) => minus(total3, 1))
+      .then((total4) => print(total4))
+      .catchError((error) => print(error.toString()));
+}
 
-  future.then((value) => print(value))
-    .catchError((error) => print("Error $error"));
+Future<int> plus(int a, int b) {
+  return Future.delayed(Duration(seconds: 1), () => a + b);
+}
+
+Future<int> minus(int a, int b) {
+  return Future.delayed(Duration(seconds: 1), () => a - b);
 }
